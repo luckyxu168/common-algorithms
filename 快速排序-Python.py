08a5e86261300fc25861d5a1c01f1589d6c6647e
luckyxu168 <45558321+luckyxu168@users.noreply.github.com>
2018-12-15 21:24:11 +0800

    Update 快速排序-Python.py

--- 快速排序-Python.py
+++ 快速排序-Python.py
@@ -1,4 +1,4 @@
-def partition(A):
+def partition(A):               #分割：把比第一个数大的数放到后面
     x = A[0]
     j = 0
     for i in range(1,len(A)):
@@ -7,20 +7,20 @@ def partition(A):
             A[j],A[i] = A[i],A[j]
     A[0],A[j] = A[j],A[0]
     return j
-def quick_sort(A):
-    if len(A)<2:
+def quick_sort(A):          #快速排序
+    if len(A)<2:            #空列表与只含一个元素的列表无需排序，直接返回
         return A
     p = partition(A)
     return quick_sort(A[0:p])+[A[p]]+quick_sort(A[p+1:len(A)])
 
-print(quick_sort([7,8,6,4,9,2,6,7,6,6,6]))
 
-import random as r
+
+
+import random as r      #随机快速排序
 def randomized_quick_sort(A):
     if len(A)<2:
         return A
-    k= r.randint(0,len(A)-1)
+    k= r.randint(0,len(A)-1)        #在列表中随机选取一个元素
     A[0],A[k]=A[k],A[0]
     p=partition(A)
     return randomized_quick_sort(A[0:p])+[A[p]]+randomized_quick_sort(A[p+1:len(A)])
-print(randomized_quick_sort([7,8,6,4,9,2,6,7,6,6,6]))
