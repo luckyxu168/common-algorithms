7771f75ca793724823cf28ea4467d6309134a175
徐威 <45558321+luckyxu168@users.noreply.github.com>
2021-01-27 13:19:40 +0800

    Update 妹子图爬虫.py

--- 妹子图爬虫.py
+++ 妹子图爬虫.py
@@ -5,20 +5,19 @@ import os
 import re
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4346.0 Safari/537.36 Edg/89.0.731.0"}
-
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"}
 
 def update_header(referer):
     headers['Referer'] = '{}'.format(referer)
 
 def dic_path(s):
-    #windows文件夹命名不能有符号
-    sn=""
+    # windows文件夹命名不能有符号
+    sn = ""
     for i in s:
-        if i in ['?','！','&']:
+        if i in ['?', '！', '&']:
             continue
         else:
-            sn+=i
+            sn += i
     return sn
 
 url = input('请输入第一页的网址：')
@@ -26,18 +25,6 @@ url = input('请输入第一页的网址：')
 res = r.get(url=url, headers=headers)
 page_text = res.text
 soup = BeautifulSoup(page_text, 'lxml')
-# 创建文件夹，下载第一页的主图片并保存到文件夹中
-page1 = str(soup.find('img', class_='blur'))
-l = page1.split('"')
-kwd = os.getcwd()
-s_d = dic_path(l[1])
-os.mkdir(kwd + '\\' + s_d)
-image_url = l[7]
-img_path = kwd + '\\' + s_d + '\\' + '1.jpg'
-update_header(url)
-img_data = r.get(url=image_url, headers=headers).content
-with open(img_path, 'wb') as fp:
-    fp.write(img_data)
 # 获取总页数
 pagenumber_moban = r'<span>\d\d</span>'
 Pattern = re.compile(pagenumber_moban)
@@ -49,6 +36,21 @@ else:
     Pattern = re.compile(pagenumber_moban)
     match = re.search(Pattern, str(soup))
     pagenumbers = int(match.group()[6:9])
+print("共有{}张图片".format(pagenumbers))
+# 创建文件夹，下载第一页的主图片并保存到文件夹中
+page1 = str(soup.find('img', class_='blur'))
+l = page1.split('"')
+kwd = os.getcwd()
+s_d = dic_path(l[1])
+os.mkdir(kwd + '\\' + s_d)
+image_url = l[7]
+img_path = kwd + '\\' + s_d + '\\' + '1.jpg'
+update_header(url)
+img_data = r.get(url=image_url, headers=headers).content
+print('正在下载第1张图片......')
+with open(img_path, 'wb') as fp:
+    fp.write(img_data)
+print('第1张图片下载完成!')
 # 下载其余的图片
 for p in range(2, pagenumbers + 1):
     url_later = url + '/' + str(p)
@@ -60,7 +62,9 @@ for p in range(2, pagenumbers + 1):
     l = page1.split('"')
     image_url = l[7]
     img_path = kwd + '\\' + s_d + '\\' + str(p) + '.jpg'
+    print("正在下载第{}张图片......".format(p))
     img_data = r.get(url=image_url, headers=headers).content
     with open(img_path, 'wb') as fp:
         fp.write(img_data)
-print('完成！')
+    print("第{}张图片下载完成!".format(p))
+print("全部{}张图片下载完成！".format(pagenumbers))
